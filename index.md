a0a76326a45ecf6cce500563a7f2244b20d481d0
fragmentbyte <fragmentbyte@protonmail.com>
2017-08-12 10:59:15 +0300

    start

---
+++ index.md
@@ -0,0 +1,11 @@
+---
+title: УРОКИ
+layout: main
+links:
+  - name: GitHub
+    link: https://github.com/0bwave/lessons
+  - name: Telegram
+    link: https://t.me/binarywave
+---
+
+Скоро...
---
+++ wikic.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: '0b',
+  desc: 'Программирование для домашнего творчества',
+  toc: {
+    selectors: 'h2, h3, h4',
+  },
+  pages: {
+    'about': 'О проекте',
+    'support': 'Поддержать'
+  },
+  excludes: ['README.md']
+};
